1a7bf1d36faebf63ee9ec64cce7a44a7b6ad2e55
Ilya Tumanov <persikabricos@gmail.com>
2022-11-28 00:29:14 +0300

    добавил тест загрузки изображений с их номерами

--- tests/test_model.py
+++ tests/test_model.py
@@ -3,10 +3,13 @@ from math import radians
 import numpy as np
 from numpy.testing import assert_array_equal
 import cv2
+from unittest.mock import patch, Mock
+from pathlib import Path
 
 from model.volume_calculation import rotate_vector_by_axis
 from model.business_logic import Model
 from model.curve_detection import detect_contours
+from model.data_interface import get_files_with_numbers
 
 
 def test_rotate_vector():
@@ -27,3 +30,20 @@ def test(num_regression):
     contours: np.ndarray = detect_contours(test_img,  lower_hsv=[1, 0, 0], upper_hsv=[22, 255, 255],
                                          threshold=254, approximation_rate=0.00135)
     num_regression.check({'mock_dict': contours.ravel()})
+
+def test_data_interface():
+    with patch.object(Path, 'iterdir') as mock_iterdir:
+        files = []
+        for name in 'img_2.jpg', 'img_1.jpg', '2im_3.jpg', 'img_no_num.jpg':
+            file = Mock()
+            file.name = name
+            files.append(file)
+        mock_iterdir.return_value = files
+        with patch('os.chdir'):
+            with patch('cv2.imread'):
+                num_images = get_files_with_numbers('')
+                nums_correct = [image.number == num for image, num in zip(num_images, (2, 1, 3))]
+                if len(nums_correct) > 0:
+                    assert all(nums_correct)
+                else:
+                    assert False
